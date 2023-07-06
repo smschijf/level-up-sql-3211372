1f2ea26768831fd6464be8b7f267d8c4061d7fce
Sander Schijf <s.mschijf@casema.nl>
2023-07-06 12:31:34 +0000

    completed library challenges

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,13 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT
+  (SELECT COUNT(Books.Title) 
+    FROM Books 
+    WHERE Books.Title = 'Dracula') - 
+  (SELECT COUNT(Books.Title)
+    FROM Books 
+    JOIN Loans ON Books.BookID = Loans.BookID 
+    WHERE Books.Title = 'Dracula' 
+    AND Loans.ReturnedDate IS NULL)
+  AS AvailableCopies;
--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,15 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT Published, COUNT(DISTINCT(Title)) AS PubCount 
+FROM Books 
+GROUP BY Published
+ORDER BY PubCount DESC;
+
+SELECT COUNT(Loans.LoanID) AS LoanCount, Books.Title 
+FROM Loans 
+JOIN Books ON Loans.BookID=Books.BookID 
+GROUP BY Books.Title 
+ORDER BY LoanCount DESC 
+LIMIT 5;
